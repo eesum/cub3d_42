5671e32a5bbe5a179eb778242f9a25d40f2a62cc
Sumin Lee <suminn0707@gmail.com>
2024-04-26 15:52:17 +0900

    feat: util 함수들 추가
    - malloc error시 exit 하는 strdup 추가
    - char ** 를 free하는 함수 추가

--- include/util.h
+++ include/util.h
@@ -6,7 +6,7 @@
 /*   By: sumilee <sumilee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 10:51:05 by sumilee           #+#    #+#             */
-/*   Updated: 2024/04/26 12:27:58 by sumilee          ###   ########.fr       */
+/*   Updated: 2024/04/26 14:56:31 by sumilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,7 @@
 
 void	error_exit(char *msg);
 void	*ft_malloc_err(size_t size);
+char	*ft_strdup_err(const char *s1);
+void	free_all(char **arg);
 
 #endif
--- util.c
+++ util.c
@@ -6,7 +6,7 @@
 /*   By: sumilee <sumilee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 10:50:33 by sumilee           #+#    #+#             */
-/*   Updated: 2024/04/26 10:54:04 by sumilee          ###   ########.fr       */
+/*   Updated: 2024/04/26 14:56:22 by sumilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,42 @@ void	*ft_malloc_err(size_t size)
 	if (ptr == NULL)
 		error_exit("Malloc failed.");
 	return ((void *)ptr);
+}
+
+static char	*ft_strcpy(char *dest, const char *src)
+{
+	size_t	i;
+	size_t	src_len;
+
+	i = 0;
+	src_len = ft_strlen(src);
+	while (i < src_len + 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+char	*ft_strdup_err(const char *s1)
+{
+	char	*dest;
+
+	dest = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (dest == NULL)
+		error_exit("Malloc failed.");
+	return (ft_strcpy(dest, s1));
+}
+
+void	free_all(char **arg)
+{
+	int i;
+
+	i = 0;
+	while (arg[i])
+	{
+		free(arg[i]);
+		i++;
+	}
+	free(arg);
 }
