80d5f622072264f4be12045121ef4e61e00b0f00
Sumin Lee <suminn0707@gmail.com>
2024-04-26 13:23:02 +0900

    fix: util.h에 누락된 util 함수 선언 추가

--- include/util.h
+++ include/util.h
@@ -6,7 +6,7 @@
 /*   By: sumilee <sumilee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 10:51:05 by sumilee           #+#    #+#             */
-/*   Updated: 2024/04/26 10:56:00 by sumilee          ###   ########.fr       */
+/*   Updated: 2024/04/26 12:27:58 by sumilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,8 @@
 # include "libft.h"
 # include <unistd.h>
 # include <stdlib.h>
+
+void	error_exit(char *msg);
+void	*ft_malloc_err(size_t size);
 
 #endif
